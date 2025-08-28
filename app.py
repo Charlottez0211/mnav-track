a1ab80ceccff1922432493ee3c3199d9ed967a85
Charlottez0211 <qianhui.zhao0211@gmail.com>
2025-08-28 19:41:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ DEFAULT_STOCK_CONFIG = {
     },
     'BMNR': {
         'shares_outstanding': 221515180,  # 默认股本数量
-        'eth_holdings': 1713899.0,           # 默认ETH持仓量
+        'eth_holdings': 1792690.0,           # 默认ETH持仓量
         'symbol': 'BMNR'
     }
 }
